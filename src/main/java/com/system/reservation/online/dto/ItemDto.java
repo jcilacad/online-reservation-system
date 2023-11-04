0c81789f2a60acf03c89a47942ca724a01bdfa83
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-04 13:00:23 +0800

    Initialize ItemDto and add getter and setter for entity/item

---
+++ src/main/java/com/system/reservation/online/dto/ItemDto.java
@@ -0,0 +1,18 @@
+package com.system.reservation.online.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ItemDto {
+
+
+    private String photos;
+
+
+}
--- src/main/java/com/system/reservation/online/entity/Item.java
+++ src/main/java/com/system/reservation/online/entity/Item.java
@@ -2,12 +2,16 @@ package com.system.reservation.online.entity;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "items")
 @AllArgsConstructor
 @NoArgsConstructor
+@Getter
+@Setter
 public class Item {
 
     @Id
