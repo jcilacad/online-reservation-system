6569c1849194a39d8c241a59941a51b017f9e9a5
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-04 13:05:17 +0800

    Add validations on itemDto

--- src/main/java/com/system/reservation/online/dto/ItemDto.java
+++ src/main/java/com/system/reservation/online/dto/ItemDto.java
@@ -1,5 +1,8 @@
 package com.system.reservation.online.dto;
 
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.Digits;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,7 +15,22 @@ import lombok.Setter;
 public class ItemDto {
 
 
+    @NotEmpty
     private String photos;
 
+    @NotEmpty
+    private String name;
+
+    @NotEmpty
+    private String size;
+
+    @DecimalMin("0.0")
+    @NotEmpty
+    private Double price;
+
+    @NotEmpty
+    private Integer quantity;
+
+    private String description;
 
 }
