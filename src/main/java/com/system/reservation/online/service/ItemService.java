ba23624928802f042a8795d6ebaa3cdc239ce3b1
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-05 09:26:15 +0800

    Add method signature for getting items in DB

--- src/main/java/com/system/reservation/online/service/ItemService.java
+++ src/main/java/com/system/reservation/online/service/ItemService.java
@@ -1,8 +1,11 @@
 package com.system.reservation.online.service;
 
 import com.system.reservation.online.dto.ItemDto;
+import com.system.reservation.online.entity.Item;
 
 public interface ItemService {
 
     Long saveItem(ItemDto itemDto, String fileName);
+
+    List<Item> findAll();
 }
