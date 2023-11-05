f0b2c2aa1796fda606ed32714b3a2a2afb2f60eb
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-05 11:38:00 +0800

    Add abstract and implementation methods for getting for pagination

--- src/main/java/com/system/reservation/online/service/ItemService.java
+++ src/main/java/com/system/reservation/online/service/ItemService.java
@@ -2,6 +2,8 @@ package com.system.reservation.online.service;
 
 import com.system.reservation.online.dto.ItemDto;
 import com.system.reservation.online.entity.Item;
+import com.system.reservation.online.entity.User;
+import org.springframework.data.domain.Page;
 
 import java.util.List;
 
@@ -10,4 +12,8 @@ public interface ItemService {
     Long saveItem(ItemDto itemDto, String fileName);
 
     List<Item> findAll();
+
+    Page<Item> findAllPaginated(Integer currentPage, Integer pageSize);
+
+    Page<Item> findItemByNameContaining(String name, Integer currentPage, Integer pageSize);
 }
