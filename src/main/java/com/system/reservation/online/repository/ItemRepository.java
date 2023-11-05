1715217c85ebf1efc60ddc779d9b552c407e5bd5
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-05 11:39:36 +0800

    Implement an abstract method for getting the list of items based on query name

--- src/main/java/com/system/reservation/online/repository/ItemRepository.java
+++ src/main/java/com/system/reservation/online/repository/ItemRepository.java
@@ -4,7 +4,10 @@ import com.system.reservation.online.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ItemRepository extends JpaRepository<Item, Long> {
 
+    List<Item> findByNameContainingIgnoreCase(String name);
 }
