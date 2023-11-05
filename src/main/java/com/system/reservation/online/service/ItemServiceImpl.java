3ed4cb4590f2e98415899325796344125ef7c198
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-05 11:45:35 +0800

    Implement the signature / abstract method to the ItemServiceImpl class

--- src/main/java/com/system/reservation/online/service/ItemServiceImpl.java
+++ src/main/java/com/system/reservation/online/service/ItemServiceImpl.java
@@ -2,8 +2,12 @@ package com.system.reservation.online.service;
 
 import com.system.reservation.online.dto.ItemDto;
 import com.system.reservation.online.entity.Item;
+import com.system.reservation.online.entity.User;
 import com.system.reservation.online.repository.ItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -39,5 +43,19 @@ public class ItemServiceImpl implements ItemService{
     @Override
     public List<Item> findAll() {
         return itemRepository.findAll();
+    }
+
+    @Override
+    public Page<Item> findAllPaginated(Integer currentPage, Integer pageSize) {
+        return itemRepository.findAll(PageRequest.of(currentPage, pageSize));
+    }
+
+    @Override
+    public Page<Item> findItemByNameContaining(String name, Integer currentPage, Integer pageSize) {
+        List<Item> userList = itemRepository.findByNameContainingIgnoreCase(name);
+
+        Page<Item> page = new PageImpl<>(userList, PageRequest.of(currentPage, pageSize), userList.size());
+
+        return page;
     }
 }
