aa6df01acfe9d4ff2cc87c28f190344589237971
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-10 19:57:58 +0800

    Initialize item.html for student

--- src/main/java/com/system/reservation/online/controller/StudentController.java
+++ src/main/java/com/system/reservation/online/controller/StudentController.java
@@ -66,7 +66,7 @@ public class StudentController {
 
         // Get item by id
         Item item = itemService.findById(itemId);
-        
+
         model.addAttribute("item", item);
 
         return null;
