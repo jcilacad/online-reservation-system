db27b5dd6d3e87fb8299120062dc1b619d4a187d
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-11 21:38:33 +0800

    Add essential properties for transaction entity

--- src/main/java/com/system/reservation/online/entity/Transaction.java
+++ src/main/java/com/system/reservation/online/entity/Transaction.java
@@ -1,9 +1,21 @@
 package com.system.reservation.online.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 @Entity
 @Table(name = "transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "received_date")
+    private String receivedDate;
+
+    @Column(name = "quantity")
+    private Integer quantity;
+
+
 }
