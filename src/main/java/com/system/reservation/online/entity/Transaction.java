f62c1683d793c72e449aa5b745e0aeb16c18efdb
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-11 22:07:22 +0800

    Fix bug for the value of attribute name for @JoinColumn

--- src/main/java/com/system/reservation/online/entity/Transaction.java
+++ src/main/java/com/system/reservation/online/entity/Transaction.java
@@ -26,7 +26,7 @@ public class Transaction {
     private Integer quantity;
 
     @ManyToOne
-    @JoinColumn(name = "id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
 
