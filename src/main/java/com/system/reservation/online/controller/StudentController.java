47b1a5f153f289e7072f2ddde0f2299b80381a78
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-14 02:07:43 +0800

    Add reservation dto in model

--- src/main/java/com/system/reservation/online/controller/StudentController.java
+++ src/main/java/com/system/reservation/online/controller/StudentController.java
@@ -1,5 +1,6 @@
 package com.system.reservation.online.controller;
 
+import com.system.reservation.online.dto.ReservationDto;
 import com.system.reservation.online.entity.Item;
 import com.system.reservation.online.service.ItemService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -58,6 +59,9 @@ public class StudentController {
     public String getItem (@PathVariable Long itemId,
                            Model model) {
 
+        // Instantiate ReservationDto
+        ReservationDto reservationDto = new ReservationDto();
+
         // Get item by id
         Item item = itemService.findById(itemId);
 
@@ -66,6 +70,7 @@ public class StudentController {
                 .boxed()
                 .collect(Collectors.toList());
 
+        model.addAttribute("reservationDto", reservationDto);
         model.addAttribute("item", item);
         model.addAttribute("reservationCount", reservationCount);
 
