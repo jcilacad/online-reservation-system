3de5c427702e15a089f27a09e3d862d058f79120
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-15 01:18:06 +0800

    Inject UserService to TranscationServiceImpl

--- src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
+++ src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
@@ -1,13 +1,33 @@
 package com.system.reservation.online.service;
 
 import com.system.reservation.online.dto.ReservationDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionServiceImpl implements TransactionService{
 
+    private UserService userService;
+
+    @Autowired
+    public TransactionServiceImpl(UserService userService) {
+        this.userService = userService;
+    }
+
     @Override
     public void reserveItem(ReservationDto reservationDto, Long itemId) {
-        
+
+        // Get current authenticated user
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        // Get email
+        String email = (String) auth.getPrincipal();
+
+
+
+
+
     }
 }
