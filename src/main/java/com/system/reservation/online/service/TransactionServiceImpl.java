96887800a19098b89e9a1ee6b78afbb14868f0a8
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-15 01:15:39 +0800

    Implement reserve item signature method

--- src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
+++ src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
@@ -1,10 +1,13 @@
 package com.system.reservation.online.service;
 
+import com.system.reservation.online.dto.ReservationDto;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TransactionServiceImpl implements TransactionService{
 
-
-
+    @Override
+    public void reserveItem(ReservationDto reservationDto, Long itemId) {
+        
+    }
 }
