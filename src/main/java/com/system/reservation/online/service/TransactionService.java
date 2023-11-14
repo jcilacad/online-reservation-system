f08e3888421ecc52e03cfdd072647b5211115635
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-15 01:15:12 +0800

    Add reserveItem method signature

--- src/main/java/com/system/reservation/online/service/TransactionService.java
+++ src/main/java/com/system/reservation/online/service/TransactionService.java
@@ -1,4 +1,8 @@
 package com.system.reservation.online.service;
 
+import com.system.reservation.online.dto.ReservationDto;
+
 public interface TransactionService {
+
+    void reserveItem(ReservationDto reservationDto, Long itemId);
 }
--- src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
+++ src/main/java/com/system/reservation/online/service/TransactionServiceImpl.java
@@ -1,4 +1,10 @@
 package com.system.reservation.online.service;
 
-public class TransactionServiceImpl {
+import org.springframework.stereotype.Service;
+
+@Service
+public class TransactionServiceImpl implements TransactionService{
+
+
+
 }
