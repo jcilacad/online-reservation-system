ab6fdf3369c82c7a1df8b447d3875c3d00184483
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-17 02:05:00 +0800

    Add recordTransactions for transactions module

--- src/main/java/com/system/reservation/online/aop/LoggingAspect.java
+++ src/main/java/com/system/reservation/online/aop/LoggingAspect.java
@@ -1,9 +1,14 @@
 package com.system.reservation.online.aop;
 
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 
 @Aspect
 public class LoggingAspect {
 
-    
+    @AfterReturning("* com.system.reservation.online.service.TransactionServiceImpl.reserveItem(..)")
+    public void recordTransactions() {
+
+        System.out.println("Reserve an item!!!");
+    }
 }
