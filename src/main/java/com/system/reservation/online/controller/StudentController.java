93da31f8a3537f818cd0b38fe3bff87c90ead6f1
jcilacad <johnchristopherilacad27@gmail.com>
2023-11-18 01:18:05 +0800

    Retrieve tansactions from TransactionsService to StudentController

--- src/main/java/com/system/reservation/online/controller/StudentController.java
+++ src/main/java/com/system/reservation/online/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.system.reservation.online.controller;
 
 import com.system.reservation.online.dto.ReservationDto;
 import com.system.reservation.online.entity.Item;
+import com.system.reservation.online.entity.Transaction;
 import com.system.reservation.online.service.ItemService;
 import com.system.reservation.online.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -103,6 +104,7 @@ public class StudentController {
     public String viewTransactions() {
 
         // Get transactions
+        List<Transaction> transactions = transactionService.viewTransactions();
 
         return "student/transactions";
     }
